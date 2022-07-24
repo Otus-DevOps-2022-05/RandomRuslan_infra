8af82c99ba5f7fa3d253c1524cc28ea3522f9182
Ruslan Shaymardanov <r.shaymardanov@vk.team>
2022-07-24 21:55:36 +0300

    terraform modules for prod and stage

--- terraform/prod/variables.tf
+++ terraform/prod/variables.tf
@@ -27,3 +27,11 @@ variable app_count {
   description = "Count"
   default     = 1
 }
+variable app_disk_image {
+  description = "Disk image for reddit app"
+  default     = "reddit-app-base"
+}
+variable db_disk_image {
+  description = "Disk image for reddit db"
+  default     = "reddit-db-base"
+}
