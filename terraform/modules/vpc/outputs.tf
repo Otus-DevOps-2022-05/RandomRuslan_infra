be0949191bb0c30f22ad1186ea647021400ea66f
Ruslan Shaymardanov <r.shaymardanov@vk.team>
2022-07-26 19:07:29 +0300

    vpc subnet in module

---
+++ terraform/modules/vpc/outputs.tf
@@ -0,0 +1,3 @@
+output "id" {
+  value = yandex_vpc_subnet.app-subnet.id
+}
--- terraform/stage/outputs.tf
+++ terraform/stage/outputs.tf
@@ -4,3 +4,6 @@ output "external_ip_address_app" {
 output "external_ip_address_db" {
   value = module.db.external_ip_address_db
 }
+output "vpc_subnet_id" {
+  value = module.subnet.id
+}
