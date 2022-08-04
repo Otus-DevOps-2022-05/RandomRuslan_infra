fcf3bb8fcd5d13a327d6de8adc628949f57fca31
Ruslan Shaymardanov <r.shaymardanov@vk.team>
2022-08-04 21:11:48 +0300

    ip hardcode

--- ansible/inventory.sh
+++ ansible/inventory.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ../terraform/stage
-APP_IP=$(terraform output external_ip_address_app)
-DB_IP=$(terraform output external_ip_address_db)
+APP_IP=51.250.65.44 # $(terraform output external_ip_address_app)
+DB_IP=51.250.82.38 #$(terraform output external_ip_address_db)
 
 echo "{\"app\": {\"hosts\": [\"$APP_IP\"]}, \"db\": {\"hosts\": [\"$DB_IP\"]}}"
