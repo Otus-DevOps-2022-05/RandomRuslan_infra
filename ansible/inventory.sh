11cf21a6ccb32892a6b6e84f04c812370e054d65
Ruslan Shaymardanov <r.shaymardanov@vk.team>
2022-08-04 21:18:44 +0300

    refactoring

--- ansible/inventory.sh
+++ ansible/inventory.sh
@@ -2,6 +2,6 @@
 
 cd ../terraform/stage
 APP_IP=51.250.65.44 # $(terraform output external_ip_address_app)
-DB_IP=51.250.82.38 #$(terraform output external_ip_address_db)
+DB_IP=51.250.82.38 # $(terraform output external_ip_address_db)
 
 echo "{\"app\": {\"hosts\": [\"$APP_IP\"]}, \"db\": {\"hosts\": [\"$DB_IP\"]}}"
