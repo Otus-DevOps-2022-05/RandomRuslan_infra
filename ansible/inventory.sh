85ed70d6a45c5d18e3e864d1b45d81c681f4b036
Ruslan Shaymardanov <r.shaymardanov@vk.team>
2022-08-04 21:05:14 +0300

    use terraform in script again

--- ansible/inventory.sh
+++ ansible/inventory.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-cd ../terraform/prod
-#APP_IP=$(terraform output external_ip_address_app)
-#DB_IP=$(terraform output external_ip_address_db)
-APP_IP=$(yc compute instance get reddit-app --format=json | python3 -c "import sys, json; print(json.load(sys.stdin)['network_interfaces'][0]['primary_v4_address']['one_to_one_nat']['address'])")
-DB_IP=$(yc compute instance get reddit-db --format=json | python3 -c "import sys, json; print(json.load(sys.stdin)['network_interfaces'][0]['primary_v4_address']['one_to_one_nat']['address'])")
+cd ../terraform/stage
+APP_IP=$(terraform output external_ip_address_app)
+DB_IP=$(terraform output external_ip_address_db)
 
 echo "{\"app\": {\"hosts\": [\"$APP_IP\"]}, \"db\": {\"hosts\": [\"$DB_IP\"]}}"
