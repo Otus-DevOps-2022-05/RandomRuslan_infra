ce11133573d1bfb8aa0b5ce746dd54bf687d3285
Ruslan Shaymardanov <r.shaymardanov@vk.team>
2022-08-08 00:13:39 +0300

    Main task: config and deploy by ansible playbooks

--- terraform/prod/outputs.tf
+++ terraform/prod/outputs.tf
@@ -4,6 +4,9 @@ output "external_ip_address_app" {
 output "external_ip_address_db" {
   value = module.db.external_ip_address_db
 }
+output "internal_ip_address_db" {
+  value = module.db.internal_ip_address_db
+}
 output "vpc_subnet_id" {
   value = module.subnet.id
 }
